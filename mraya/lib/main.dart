af95687f55ea97b6258398b1a8667e8cc43f34e2
Bushra Alzahrani <B.alzahrani@thegarage.sa>
2023-03-17 13:40:13 +0300

    feat: chat ui

--- mraya/lib/main.dart
+++ mraya/lib/main.dart
@@ -1,115 +1,107 @@
-import 'package:flutter/material.dart';
+// 
+
+
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:uuid/uuid.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Widget build(BuildContext context) => const MaterialApp(
+        home: ChatPage(),
+      );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ChatPage> createState() => _ChatPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  static const String baseURL =
-      'https://experimental.willow.vectara.io/v1/chat/completions';
-
-
- var msgRes = []; // save previous responses from the chat assistant (choices)
-
-  talkToChatGpt(String role,String userMsg) async {
-    // first save the user input in msgRes
-
-    // then the save the data response in msgRes
-
-    try {
-      final response = await http.post(Uri.parse('${baseURL}'), body:
-      json.encode( 
-       {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-          {"role": role, "content": userMsg}
-        ]
-      })
-     , headers: {
-        'customer-id': '4271855006',
-        'x-api-key': 'zqt__p9VnshHXErIL8kRx4GxGCMnyRfn0wzRclJp7Q',
-      });
-      if (response.statusCode == 200) {
-        var data = jsonDecode(response.body.toString());
-
-        // msgRes.add(data)
-        print('Added message!!!!!!!!!');
-        print(data);
-        return data;
-      } else {
-        print('error in response!!!!!!!');
-        var data = jsonDecode(response.body.toString());
-        print(data);
-     
-      }
-    } catch (e) {
-      print('error in catch!!!!!!!!');
-      print(e.toString());
-    }
-  }
-
-
-  getMsgMemory(String newUserMsg, List lastMsgs){
-
-  // send the new msg as user
-
-  // talkToChatGpt('user', newUserMsg);
-
-
-  // var msgcompletion = talkToChatGpt(lastMsgs.toString());
-  
-
-  // var msgresponse = msgcompletion.choices[0].message.content;
-  // print(msgresponse);
-
-  // return msgresponse;
-  }
-
-   
-
-
-    // var newMsg = getMsgMemory('',msgRes);
-    // msgRes.add({"role": "assistant", "content": newMsg});
-
-
+class _ChatPageState extends State<ChatPage> {
+  List<types.Message> _messages = [];
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    // talkToChatGpt('describe yourself');
+    _loadMessages();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+  Widget build(BuildContext context) => Scaffold(
+        body: Chat(
+           theme: const DefaultChatTheme(
+        inputBackgroundColor: Color.fromARGB(255, 226, 226, 226),
+        inputTextColor: Colors.grey,
+        primaryColor: Colors.grey,
+
+        
+
+      ),
+          messages: _messages,
+          // onMessageTap: _handleMessageTap,
+          onPreviewDataFetched: _handlePreviewDataFetched,
+          onSendPressed: _handleSendPressed,
+          showUserAvatars: true,
+          showUserNames: true,
+          user: _user,
         ),
-        body: Center(
-          child: Text('data'),
-        ));
+      );
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
+
+  void _handlePreviewDataFetched(
+    types.TextMessage message,
+    types.PreviewData previewData,
+  ) {
+    final index = _messages.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    setState(() {
+      _messages[index] = updatedMessage;
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: const Uuid().v4(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
+  void _loadMessages() async {
+    final response = await rootBundle.loadString('assets/messages.json');
+    final messages = (jsonDecode(response) as List)
+        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
+        .toList();
+    setState(() {
+      _messages = messages;
+    });
+  }
+
 }

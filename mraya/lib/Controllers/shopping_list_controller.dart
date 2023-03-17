ffcbd22943ce4139ccad4c5d208453a985400320
Bushra Alzahrani <B.alzahrani@thegarage.sa>
2023-03-17 20:09:32 +0300

    enh: testing dall-e

--- mraya/lib/Controllers/shopping_list_controller.dart
+++ mraya/lib/Controllers/shopping_list_controller.dart
@@ -1,28 +1,32 @@
 import 'dart:convert';
 import 'package:get/get.dart';
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:uuid/uuid.dart';
 import '../services/ikea_proudcts.dart';
-
+import 'package:http/http.dart' as http;
 import '../Models/proudct_model.dart';
 
 class ShoppingListController extends GetxController {
-
   var ikeaProductsList = <ProudctModel>[].obs;
-   var isLoading = true.obs;
+  var isLoading = true.obs;
+
+  String open_ai_api_key =
+      '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';
+
+  String baseURL = 'https://api.openai.com/v1/images/generations';
+
+  final dallEResult= ''.obs;
 
   onInit() {
-   getAllIkeaProudcts();
-   print('in onInit!!!!!!!!');
+    getAllIkeaProudcts();
+    print('in onInit!!!!!!!!');
+    sendShoppingListToDallE("3d model dining room industrial style");
   }
 
-
-    void getAllIkeaProudcts() async {
+  void getAllIkeaProudcts() async {
     var products = await IkeaProudctsService.getIkeaProudcts();
+    print('in getAllIkeaProudcts!!!!!!!!');
     try {
       isLoading(true);
       if (products.isNotEmpty) {
-
         ikeaProductsList.addAll(products);
       }
     } finally {
@@ -30,6 +34,38 @@ class ShoppingListController extends GetxController {
     }
   }
 
- 
+  Future<String?> sendShoppingListToDallE(String imgDesc)async{
 
-}
+     try {
+      final response = await http.post(Uri.parse('${baseURL}'), body: {
+      "prompt": imgDesc,
+      "n":1,
+      "size":"500x500"
+      }, headers: {
+        'Authorization': 'Bearer ${open_ai_api_key}'
+      });
+      if (response.statusCode == 200) {
+        print('dall-e Succes!!!!!!!!!!!!!!!!!!!!');
+         var data = jsonDecode(response.body.toString());
+        print('_______________________________________________${data['data']['url']}');
+
+        dallEResult.value= data['data'][0]['url'];
+
+        return data['data']['url'];
+
+      } else {
+        print('0-0-0-0 error dall-e!!!!!!!!');
+    
+      }
+    } catch (e) {
+       print('0-0-0-0 catch error dall-e!!!!!!!!');
+      print(e.toString());
+    }
+  
+  }
+
+  }
+
+
+   
+
--- mraya/lib/Views/Screens/shoppingList.dart
+++ mraya/lib/Views/Screens/shoppingList.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
 
+import '../../Controllers/shopping_list_controller.dart';
 import '../Widgets/customButton.dart';
 import '../Widgets/proudctCard.dart';
 
 class ShoppingList extends StatelessWidget {
-  const ShoppingList({super.key});
+   ShoppingList({super.key});
+
+    final controller = Get.find<ShoppingListController>();
 
   @override
   Widget build(BuildContext context) {
+    print('in screen!!!!!!!!!!!!!!!!!! ${controller.dallEResult.value}');
     return SafeArea(
         child: Scaffold(
       backgroundColor: Color(0xffbEAF0EF),
-      body: SingleChildScrollView (
-        child: Column(
-          children: [
-            GridView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-              padding: EdgeInsets.all(22.0),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 20,
-                ),
-                itemCount: 8,
-                itemBuilder: (BuildContext context, int index) {
-                  return ProudctCard();
-                }),
-                SizedBox(height: 100,),
-                CustomButton(text: 'Continue', isButtonDisabled: false,),
-                CustomButton(text: 'show me another suggestion ', isButtonDisabled: true,),
+      body: 
+      Image.network(controller.dallEResult.value),
       
-          ],
-        ),
-      ),
+      // SingleChildScrollView (
+      //   child: Column(
+      //     children: [
+      //       GridView.builder(
+      //           physics: NeverScrollableScrollPhysics(),
+      //           shrinkWrap: true,
+      //         padding: EdgeInsets.all(22.0),
+      //           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //             crossAxisCount: 2,
+      //             mainAxisSpacing: 20,
+      //             crossAxisSpacing: 20,
+      //           ),
+      //           itemCount: 8,
+      //           itemBuilder: (BuildContext context, int index) {
+      //             return ProudctCard();
+      //           }),
+      //           SizedBox(height: 100,),
+      //           CustomButton(text: 'Continue', isButtonDisabled: false,),
+      //           CustomButton(text: 'show me another suggestion ', isButtonDisabled: true,),
+      
+      //     ],
+      //   ),
+      // ),
+
     ));
   }
 }

339b4bcb7a3fb4e178143fcce2055638d494ce70
Bushra Alzahrani <B.alzahrani@thegarage.sa>
2023-03-18 07:35:32 +0300

    fix:  binding

--- mraya/lib/binding.dart
+++ mraya/lib/binding.dart
@@ -6,7 +6,7 @@ import 'Controllers/items_result_controller.dart';
 class Binding extends Bindings {
   @override
   void dependencies() {
-    // Get.put(ChatController());
-     Get.put(ItemsResultController());
+    Get.put(ChatController());
+    Get.put(ItemsResultController());
   }
 }
